372a9f0c6b969e08c57443862444f8bf4746df8b
levani vanishvili <levan.vanishvili@gmail.com>
2020-11-05 19:04:24 +0400

    Add project files.

---
+++ MyExtension/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace MyExtension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter numbers separated by commas");
+            try
+            {
+                string input = Console.ReadLine();
+
+                int[] intArray = Array.ConvertAll(input.Split(","), i => int.Parse(i));                
+                
+                var outPut = intArray.ThisDoesntMakeAnySense(i => i.Equals(10),  i => Console.WriteLine("matching number {0}", i));
+
+                if (intArray.Length == outPut.Count())
+                {
+                    for (int i = 0; i < intArray.Length; i++)
+                    {
+                        Console.WriteLine("entered number is {0}", intArray[i]);
+                    }
+                }
+            }
+            catch (Exception Ex)
+            {
+
+                Console.WriteLine(Ex.Message);
+            }
+        }
+    }
+}
---
+++ MyExtension/ThisDoesntMakeAnySenseExt.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyExtension
+{
+     static class ThisDoesntMakeAnySenseExt
+     {
+        public static IEnumerable<T> ThisDoesntMakeAnySense<T>(this IEnumerable<T> source, Func<T,bool> predicate, Action<T> recorder)
+        {
+            if (source == null) throw new ArgumentException("source");
+            if (predicate == null) throw new ArgumentException("predicate");
+            if (recorder == null) throw new ArgumentException("recorder");
+
+            List<T> list = new List<T>();
+
+            foreach (var item in source)
+            {
+                if (!predicate(item))
+                {
+                    list.Add(item);
+                }
+                else
+                {
+                    recorder(item);
+                    return list;
+                }
+            }
+            return list;
+        }
+     }
+}
